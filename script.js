7e54ffeba63f0a937d80629a7b35e33c854c756c
bosunabujade <bosunabujade@gmail.com>
2022-01-05 21:46:54 -0500

    Added the html and css styles for the header section of my home page of portfolio project

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById("hamburger");
+const closebtn = document.getElementById("closebtn");
+const mySidenav = document.getElementById("mySidenav")
+
+hamburger.addEventListener('click', () => {
+mySidenav.style.width = "100%" ;
+})
+
+closebtn.addEventListener('click', () => {
+    mySidenav.style.width = "0%" ;
+})
