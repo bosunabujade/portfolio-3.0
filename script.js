03400530e144ce6c2dd97d7511adc23cb4bfec85
bosunabujade <bosunabujade@gmail.com>
2022-01-11 23:18:16 -0500

    added project page styles

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const closebtn = document.getElementById("closebtn");
-const mySidenav = document.getElementById("mySidenav")
-
+const mySidenav = document.getElementById("mySidenav");
 
 hamburger.addEventListener('click', () => {
 mySidenav.style.width = "100%" ;
-})
+});
 
 closebtn.addEventListener('click', () => {
     mySidenav.style.width = "0%" ;
-})
+});
+
+
+  
